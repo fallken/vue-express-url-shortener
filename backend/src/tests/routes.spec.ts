6a5bc45418aea7e4720ab9fbfb5c3fc8c68142b2
faramarz <arsalani@outlook.com>
2021-02-21 16:40:51 +0330

    - updated routes test and added url redirect route test to it

--- backend/src/tests/routes.spec.ts
+++ backend/src/tests/routes.spec.ts
@@ -4,18 +4,19 @@ import { MongoConnection } from "../database/Mongodb";
 import * as mongo from "mongodb";
 
 describe("testing api routes", () => {
+  const url = "https://www.youtube.com/watch?v=Iy2ee9wiMrg";
   let db: mongo.Db;
+
   beforeAll(async (done) => {
     db = await MongoConnection.getDatabase('test_db');
     done();
   });
 
   describe("POST /api/ for generating short url", () => {
-    const url = "https://www.youtube.com/watch?v=Iy2ee9wiMrg";
 
     test("test success for adding new url", async (done) => {
       // debugger
-      await request(app).post("/api")
+      await request(app).post("/")
         .send({
           url
         }).expect(200).then((response: any) => {
@@ -25,7 +26,7 @@ describe("testing api routes", () => {
       done()
     });
     test("test failed with empty body (400)", async (done) => {
-      await request(app).post("/api")
+      await request(app).post("/")
         .send({}).expect(400).then((response: any) => {
           expect(response.body.meta.code === 400).toBeTruthy();
           expect(response.body.meta.detail.errors.length > 0).toBeTruthy();
@@ -33,13 +34,39 @@ describe("testing api routes", () => {
       done()
     });
     test("test failed with wrong url type (400)", async (done) => {
-      await request(app).post("/api")
+      await request(app).post("/")
         .send({
           url: "dummyurl"
         }).expect(400).then((response: any) => {
           expect(response.body.meta.code === 400).toBeTruthy();
           expect(response.body.meta.detail.errors.length > 0).toBeTruthy();
         })
+      done()
+    });
+  });
+
+  describe("get / list of current urls ", () => {
+    test("test success for adding new url", async (done) => {
+      // debugger
+      await request(app).get("/")
+        .expect(200).then((response: any) => {
+          expect(response.body.meta.code === 200).toBeTruthy();
+          expect(response.body.data.items.length === 1).toBeTruthy();
+          expect(response.body.data.items[0].main_url).toEqual(url);
+        })
+      done()
+    });
+  });
+
+  describe("redirect with url", () => {
+    test("fail with wrong id not found(404)", async (done) => {
+      // debugger
+      await request(app).get("/sfasf123").expect(404);
+      done()
+    });
+    test("fail test with short param(length) passed (lower than 8)", async (done) => {
+      // debugger
+      await request(app).get("/123").expect(400);
       done()
     });
   });
